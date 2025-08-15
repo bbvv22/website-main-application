2ac13e6b85404d25d083db9eb423cb157e9f7d48
Vamshi Krishna <vamshikrishna@Vamshis-MacBook-Pro.local>
2025-08-15 16:33:51 +0530

    Fix FileResponse import

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, APIRouter, FileResponse
+from fastapi import FastAPI, APIRouter
+from starlette.responses import FileResponse
 from dotenv import load_dotenv
 from starlette.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
